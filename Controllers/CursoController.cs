5384727fa3be4ffc79363ee7b1ad0c7e16bb4add
Marques <marques.maciel@escolar.ifrn.edu.br>
2023-10-31 11:04:58 -0300

    adicionado HttpDelete

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -59,5 +59,19 @@ namespace apiUniversidade.Controllers
             return Ok(curso);
         }
 
+        [HttpDelete("{id int}")]
+        public ActionResult Delete(int id){
+            var curso = _context.Cursos.FirstOrDefault(p => p.Id == id);
+
+            if (curso is null){
+                return NotFound();
+            }
+
+            _context.Cursos.Remove(curso);
+            _context.SaveChanges();
+
+            return Ok(curso);
+        }
+
     }
 }
