4279d0a963a7dc02cf4038818d10c15cdb43f8c3
Marques <marques.maciel@escolar.ifrn.edu.br>
2023-11-28 11:52:39 -0300

    identity + DTO

---
+++ DTO/UsuarioDTO.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+
+namespace apiUniversidade.DTO;
+public class UsuarioDTO
+{
+    public string Email {get;set;}
+    public string Password {get;set;}
+    public string ConfirmPassword {get;set;}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using apiUniversidade.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,10 @@ builder.Services.AddDbContext<ApiUniversidadeContext>(options => options.UseNpgs
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+.AddEntityFrameworkStores<ApiUniversidadeContext>()
+.AddDefaultTokenProviders();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +26,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
